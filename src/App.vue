e45908cd475728158947d3abaf17ef8f17dc3198
KiditsGood <71182254+KiditsGood@users.noreply.github.com>
2023-03-28 23:33:10 +0300

    Header, Wrapper, Footer INIT

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Header />
+  <Wrapper>
+    <router-view/>
+  </Wrapper>
+  <Footer />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
+<script>
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import Wrapper from "@/components/Wrapper/Wrapper";
+export default {
+  components: {Wrapper, Footer, Header}
+}
+</script>
